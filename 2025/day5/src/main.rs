22f000c7460fdb26760eb1e5439da0c11d7ac28c
Fai <me@faithefairy.com>
2025-12-23 13:06:12 -0600

    Implemented merge ranges function and we now check each range

--- 2025/day5/src/main.rs
+++ 2025/day5/src/main.rs
@@ -4,6 +4,27 @@ use std::{
     path::PathBuf,
 };
 
+/// Takes input that consists of a list of fresh ingredient ID ranges, a blank line, and a list of available ingredient IDs.
+/// # Example
+/// ```
+/// let input = "3-5
+///10-14
+///16-20
+///12-18
+///
+///1
+///5
+///8
+///11
+///17
+///32";
+///
+/// let result: (Vec<(usize, usize)>, Vec<usize>) = parse_input(input);
+/// let expected_fresh: Vec<(usize, usize)> = vec![(3, 5), (10, 14), (16, 20), (12, 18)];
+/// let expected_available: Vec<usize> = vec![1, 5, 8, 11, 17, 32];
+/// let expected = (expected_fresh, expected_available);
+/// assert_eq!(result, expected);
+/// ```
 fn parse_input(input: &str) -> (Vec<(usize, usize)>, Vec<usize>) {
     let mut fresh_ingredients_ranges: Vec<(usize, usize)> = vec![];
     let mut available_ingredients: Vec<usize> = vec![];
@@ -21,27 +42,56 @@ fn parse_input(input: &str) -> (Vec<(usize, usize)>, Vec<usize>) {
     (fresh_ingredients_ranges, available_ingredients)
 }
 
-fn main() {
-    let filename = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("input1.txt");
-    // let filename = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("input.txt");
-    let input = fs::read_to_string(filename).unwrap();
-    let (fresh_ranges, available_ids) = parse_input(&input);
-    println!("Fresh: {fresh_ranges:?}");
-    println!("Available: {available_ids:?}");
-    // let mut count_fresh_available: usize = 0;
-    // for ingredient in available {
-    //     if fresh.contains(&ingredient) {
-    //         count_fresh_available += 1;
-    //     }
-    // }
-    let mut count_fresh_available: usize = 0;
+/// Given a reference to a vector of ranges organized in tuples (start, end),
+/// this function returns a new vector of ranges that has no overlap
+/// # Example
+/// ```
+/// let raw_ranges = vec![(1, 3), (5, 19), (17, 20), (19, 23)];
+/// let merged_ranges = merge_ranges(&raw_ranges);
+/// let expected = vec![(1, 3), (5, 23)];
+/// assert_eq!(merged_ranges, expected);
+/// ```
+fn merge_ranges(raw_ranges: &[(usize, usize)]) -> Vec<(usize, usize)> {
+    let mut merged_ranges: Vec<(usize, usize)> = vec![];
+    let mut sorted_ranges = raw_ranges.to_owned();
 
-    for id in available_ids {
-        if fresh_ranges.iter().any(|(a, b)| (a..=b).contains(&&id)) {
-            count_fresh_available += 1;
+    // sort ranges in ascending order of start of each range
+    sorted_ranges.sort_unstable_by_key(|(start1, _end1)| *start1);
+
+    let &(mut start, mut end) = &sorted_ranges[0];
+    for &(next_start, next_end) in &sorted_ranges {
+        // check if next_start..next_end has overlap with start..end
+        if next_start <= end {
+            // extend end
+            end = end.max(next_end);
+        } else {
+            merged_ranges.push((start, end));
+            start = next_start;
+            end = next_end;
         }
     }
-    // let count_fresh_available: usize = available_ids.intersection(&fresh_ranges).count();
+    // push last range
+    merged_ranges.push((start, end));
+    merged_ranges
+}
+
+fn main() {
+    // let filename = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("input1.txt");
+    let filename = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    let input = fs::read_to_string(filename).unwrap();
+    let (fresh_ranges, available_ids) = parse_input(&input);
+    // println!("Fresh: {fresh_ranges:?}");
+    // println!("Available: {available_ids:?}");
+    let mut count_fresh_available: usize = 0;
+    let fresh_ranges_merged = merge_ranges(&fresh_ranges);
+
+    for (start, end) in fresh_ranges_merged {
+        for id in &available_ids {
+            if (start..=end).contains(id) {
+                count_fresh_available += 1;
+            }
+        }
+    }
     println!("Number of available ingredient IDs that are fresh = {count_fresh_available}");
 }
 
@@ -62,10 +112,18 @@ mod tests {
 
     #[test]
     fn test_parse_input() {
-        let result: (Vec<usize>, Vec<usize>) = parse_input(INPUT);
-        let expected_fresh: Vec<usize> = vec![3, 4, 5, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 15];
+        let result: (Vec<(usize, usize)>, Vec<usize>) = parse_input(INPUT);
+        let expected_fresh: Vec<(usize, usize)> = vec![(3, 5), (10, 14), (16, 20), (12, 18)];
         let expected_available: Vec<usize> = vec![1, 5, 8, 11, 17, 32];
         let expected = (expected_fresh, expected_available);
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_merge_ranges() {
+        let raw_ranges = vec![(1, 3), (5, 19), (17, 20), (19, 23)];
+        let merged_ranges = merge_ranges(&raw_ranges);
+        let expected = vec![(1, 3), (5, 23)];
+        assert_eq!(merged_ranges, expected);
     }
 }
