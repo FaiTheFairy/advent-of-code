b8daeee3bb394e9ac5be63197a7a8e9173a2184e
Fai <me@faithefairy.com>
2025-12-24 06:55:39 -0600

    2025/day6 part 1 is done.

--- 2025/day6/src/main.rs
+++ 2025/day6/src/main.rs
@@ -1,3 +1,14 @@
+use std::{fs, path::PathBuf};
+
+use day6::{calculate_columns, calculate_sum_of_results, parse_input};
+
 fn main() {
-    println!("Hello, world!");
+    let filename = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("input.txt");
+    let input = fs::read_to_string(filename).expect("Couldn't read file");
+    let columns = parse_input(&input);
+    let results = calculate_columns(columns);
+    let sum = calculate_sum_of_results(results);
+    println!(
+        "Part 1. The total found by adding all of the answers to the individual problems = {sum}"
+    );
 }
