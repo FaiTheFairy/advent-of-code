52feaff8f162b3bf39ee07f4922ccc23c8ca01da
Fai <me@faithefairy.com>
2025-12-27 12:46:39 -0600

    2025/day6 part 2 done

--- 2024/day6/src/lib.rs
+++ 2024/day6/src/lib.rs
@@ -26,7 +26,7 @@ pub fn parse_input(input: &str) -> (Vec<Vec<u8>>, (i32, i32)) {
 
 /// runs a loop simulating the guards movement, keeping track of every distinct
 /// position it walks over in a HashSet. returns the length of said HashSet
-pub fn count_distinct(grid: Vec<Vec<u8>>, start: (i32, i32)) -> usize {
+pub fn count_distinct(grid: &[Vec<u8>], start: (i32, i32)) -> usize {
     // up, right, down, left
     let dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)];
     let mut dir_idx = 0; // starts facing up
@@ -57,6 +57,62 @@ pub fn count_distinct(grid: Vec<Vec<u8>>, start: (i32, i32)) -> usize {
         }
     }
     visited.len()
+}
+
+pub fn loops_with_obstruction(grid: &[Vec<u8>], start: (i32, i32), block: (i32, i32)) -> bool {
+    let dirs = [(0, -1), (1, 0), (0, 1), (-1, 0)];
+    let mut dir_idx = 0usize;
+
+    let mut pos = start;
+    let height = grid.len() as i32;
+    let width = grid[0].len() as i32;
+
+    let mut seen: HashSet<(i32, i32, usize)> = HashSet::new();
+
+    loop {
+        if !seen.insert((pos.0, pos.1, dir_idx)) {
+            return true; // loop
+        }
+
+        let (dx, dy) = dirs[dir_idx];
+        let next = (pos.0 + dx, pos.1 + dy);
+
+        if next.0 < 0 || next.1 < 0 || next.0 >= width || next.1 >= height {
+            return false; // exited map
+        }
+
+        let blocked_by_wall = grid[next.1 as usize][next.0 as usize] == b'#';
+        let blocked_by_new = next == block;
+
+        if blocked_by_wall || blocked_by_new {
+            dir_idx = (dir_idx + 1) % 4;
+        } else {
+            pos = next;
+        }
+    }
+}
+
+pub fn count_loop_positions(grid: &[Vec<u8>], start: (i32, i32)) -> usize {
+    let height = grid.len();
+    let width = grid[0].len();
+    let mut count = 0;
+
+    for y in 0..height {
+        for x in 0..width {
+            let p = (x as i32, y as i32);
+
+            if p == start {
+                continue;
+            }
+            if grid[y][x] == b'#' {
+                continue;
+            }
+            if loops_with_obstruction(grid, start, p) {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 #[cfg(test)]
@@ -99,8 +155,16 @@ mod tests {
     #[test]
     fn test_count_distinct() {
         let (grid, start) = parse_input(EXAMPLE);
-        let result = count_distinct(grid, start);
+        let result = count_distinct(&grid, start);
         let expected = 41usize;
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_count_loop_positions() {
+        let (grid, start) = parse_input(EXAMPLE);
+        let result = count_loop_positions(&grid, start);
+        let expected = 6usize;
         assert_eq!(result, expected);
     }
 }
--- 2024/day6/src/main.rs
+++ 2024/day6/src/main.rs
@@ -1,11 +1,16 @@
 use std::{fs, path::PathBuf};
 
-use day6::{count_distinct, parse_input};
+use day6::{count_distinct, count_loop_positions, parse_input};
 
 fn main() {
     let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("input.txt");
     let input = fs::read_to_string(path).expect("Couldn't read input file");
     let (grid, start) = parse_input(&input);
-    let count_distinct = count_distinct(grid, start);
+    let count_distinct = count_distinct(&grid, start);
     println!("Part 1. The number of distinct tiles the guard will walk over = {count_distinct}");
+
+    let count_loop_positions = count_loop_positions(&grid, start);
+    println!(
+        "Part 2. Different positions an obstruction would cause an the guard to go in an infinite loop = {count_loop_positions}"
+    );
 }
